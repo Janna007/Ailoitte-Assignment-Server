f56b939edf1552ea6f38a262a8f9cbee9ae84ee2
Janna007 <jannakondeth5@gmail.com>
2025-11-27 19:06:18 +0530

    add API for delete products

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -6,12 +6,11 @@ import { ProductController } from '../controller/ProductController';
 import { upload } from '../middleware/multer';
 import { ProductService } from '../services/productService';
 
-
 const router = express.Router();
 
-const productService=new ProductService()
+const productService = new ProductService();
 
-const productController = new ProductController(logger,productService);
+const productController = new ProductController(logger, productService);
 
 router.post(
     '/',
@@ -19,29 +18,35 @@ router.post(
     canAccess(['admin']),
     upload.fields([
         {
-            name:"product",
-            maxCount:5
+            name: 'product',
+            maxCount: 5,
         },
-    ])  ,
+    ]),
     (req: Request, res: Response, next: NextFunction) =>
         productController.createProduct(req, res, next)
 );
 
-// router.patch(
-//     '/:id',
-//     authenticate,
-//     canAccess(['admin']),
-//     (req: Request, res: Response, next: NextFunction) =>
-//         categoryController.updateCategory(req, res, next)
-// );
+router.patch(
+    '/:id',
+    authenticate,
+    canAccess(['admin']),
+    upload.fields([
+        {
+            name: 'product',
+            maxCount: 5,
+        },
+    ]),
+    (req: Request, res: Response, next: NextFunction) =>
+        productController.updateProduct(req, res, next)
+);
 
-// router.delete(
-//     '/:id',
-//     authenticate,
-//     canAccess(['admin']),
-//     (req: Request, res: Response, next: NextFunction) =>
-//         categoryController.deleteCategory(req, res, next)
-// );
+router.delete(
+    '/:id',
+    authenticate,
+    canAccess(['admin']),
+    (req: Request, res: Response, next: NextFunction) =>
+        productController.deleteProduct(req, res, next)
+);
 
 router.get('/', authenticate, (req: Request, res: Response, next: NextFunction) =>
     productController.getProducts(req, res, next)
--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,9 +1,9 @@
-import createHttpError from "http-errors";
-import {Category, Product} from "../models";
-import { ProductInput } from "../types";
+import createHttpError from 'http-errors';
+import { Category, Product } from '../models';
+import { ProductInput } from '../types';
 
-export class ProductService{
-    async create( {name, description, price, stock, categoryId,imageUrl}:ProductInput){
+export class ProductService {
+    async create({ name, description, price, stock, categoryId, imageUrl }: ProductInput) {
         try {
             return await Product.create({
                 name,
@@ -11,32 +11,79 @@ export class ProductService{
                 price,
                 stock,
                 categoryId,
-                imageUrl
+                imageUrl,
             });
-        } catch (err){
-            console.log(err)
+        } catch (err) {
+            console.log(err);
             const error = createHttpError(500, 'Error while saving data to the database');
             throw error;
         }
     }
 
-    async getList(query:any,pageLimit:number,offset:number) {
+    async getList(query: any, pageLimit: number, offset: number) {
         try {
             return await Product.findAndCountAll({
-                where:query,
-                limit:pageLimit,
+                where: query,
+                limit: pageLimit,
                 offset,
-                include:[
+                include: [
                     {
-                        model:Category,
-                        attributes: ["id", "name", "description"],
-                    }
+                        model: Category,
+                        attributes: ['id', 'name', 'description'],
+                    },
                 ],
-                order: [["createdAt", "DESC"]],
+                order: [['createdAt', 'DESC']],
             });
         } catch (err) {
             console.error('product listing error:', err);
             const error = createHttpError(500, 'Error while listing  products');
+            throw error;
+        }
+    }
+
+    async delete(productId: number) {
+        try {
+            const product = await Product.findByPk(productId);
+            // console.log(category);
+            if (!product) {
+                throw createHttpError(404, 'Category not found');
+            }
+
+            await product.destroy();
+        } catch (err) {
+            console.error('product delete error:', err);
+            const error = createHttpError(500, 'Error while deleting  product');
+            throw error;
+        }
+    }
+
+    async update(
+        { name, description, price, stock, categoryId, imageUrl }: ProductInput,
+        productId: number
+    ) {
+        try {
+            const product = await Product.findByPk(productId);
+
+            if (!product) {
+                throw createHttpError(404, 'Product not found');
+            }
+
+            product.name = name ?? product.name;
+            product.price = price ?? product.price;
+            product.stock = stock ?? product.stock;
+            product.categoryId = categoryId ?? product.categoryId;
+
+            if (description) {
+                product.description = description ?? product.description;
+            }
+            product.imageUrl = imageUrl ?? product.imageUrl;
+
+            await product.save();
+
+            return product;
+        } catch (err) {
+            console.error('Category update error:', err);
+            const error = createHttpError(500, 'Error while saving data to the database');
             throw error;
         }
     }
