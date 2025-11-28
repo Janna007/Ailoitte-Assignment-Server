49d2e87b59ec5a28cb3e219e53b5cd609b2fbdac
Janna007 <jannakondeth5@gmail.com>
2025-11-28 15:20:30 +0530

    fix build error

---
+++ src/types/multer.d.ts
@@ -0,0 +1,14 @@
+import { Multer } from 'multer';
+
+declare global {
+    namespace Express {
+        interface Request {
+            file?: Multer.File;
+            files?: {
+                [fieldname: string]: Multer.File[];
+            };
+        }
+    }
+}
+
+export {};
